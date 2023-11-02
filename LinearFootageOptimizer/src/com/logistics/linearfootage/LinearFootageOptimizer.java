b470e0c10088f1909250a998ae942e5b5a6c72ad
akhil <akhil@10.0.0.179>
2023-11-01 20:07:44 -0400

    Adding initial project files and base class to repository

---
+++ LinearFootageOptimizer/src/com/logistics/linearfootage/LinearFootageOptimizer.java
@@ -0,0 +1,11 @@
+package com.logistics.linearfootage;
+
+public class LinearFootageOptimizer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Linear Footage Optimizer Project!");
+
+	}
+
+}
---
+++ LinearFootageOptimizer/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module LinearFootageOptimizer {
+}
