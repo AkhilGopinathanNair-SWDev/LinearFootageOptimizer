f60e2afc9ee968d5b14f3c8a11a7cabf8e669cc0
akhil <akhil@10.0.0.179>
2023-11-02 11:46:37 -0400

    Implementing Linear Footage Result class to hold the linear footage value and a list of LineItems.

---
+++ LinearFootageOptimizer/src/com/logistics/linearfootage/model/LinearFootageResult.java
@@ -0,0 +1,36 @@
+/**
+ * Represents a Linear Footage Result in the context of logistics and freight.
+ * This class encapsulates the rounded linear footage value and a list of LineItems,
+ * providing a summary of dimensions, weight, and properties of a shipment unit.
+ * The rounded linear footage represents the total length of items in linear feet,
+ * while the LineItems provide detailed information about individual items within the shipment.
+ *
+ * @author Akhil Gopinathan Nair
+ * @version 1.0
+ * @since 2023-11-02
+ */
+
+package com.logistics.linearfootage.model;
+
+import java.util.List;
+
+public class LinearFootageResult {
+	private int roundedLinearFootage;
+    private List<LineItem> lineItems;
+
+    public LinearFootageResult(int roundedLinearFootage, List<LineItem> lineItems) {
+        this.roundedLinearFootage = roundedLinearFootage;
+        this.lineItems = lineItems;
+    }
+
+    public LinearFootageResult() {
+	}
+
+	public int getRoundedLinearFootage() {
+        return roundedLinearFootage;
+    }
+
+    public List<LineItem> getLineItems() {
+        return lineItems;
+    }
+}
