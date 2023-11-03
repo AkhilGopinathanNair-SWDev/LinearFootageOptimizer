6420d73277cf450c7914d708704d9f660230eb0b
akhil <akhil@10.0.0.179>
2023-11-02 21:44:57 -0400

    Implementing Stackable logic in Linear Footage Optimizer algorithm

--- LinearFootageOptimizer/src/com/logistics/linearfootage/util/LinearFootageOptimizer.java
+++ LinearFootageOptimizer/src/com/logistics/linearfootage/util/LinearFootageOptimizer.java
@@ -20,6 +20,7 @@ public class LinearFootageOptimizer {
 	public int calculateLinearFootage(List<LineItem> lineItems, int truckWidthInches, int truckHeightInches) {
 		double totalLinearFootage = 0.0;
 		double totalWidthInches = 0.0;
+		boolean alreadyStacked = false;
 		boolean firstColumn = true;
 
 		for (LineItem item : lineItems) {
@@ -33,17 +34,26 @@ public class LinearFootageOptimizer {
 			} else {
 				totalWidthInches = truckWidthInches; // Takes up entire width
 			}
-
-			if (item.getWidthInches() > truckWidthInches / 2 && firstColumn) {
-				// If it takes up more than 50% width, consider it taking up the entire width
-				totalWidthInches = truckWidthInches;
-				totalLinearFootage += itemLinearFootage;
+			if (!item.isStackable()) {
+				if (item.getWidthInches() > truckWidthInches / 2 && firstColumn) {
+					// If it takes up more than 50% width, consider it taking up the entire width
+					totalWidthInches = truckWidthInches;
+					totalLinearFootage += itemLinearFootage;
+				} else {
+					totalLinearFootage += itemLinearFootage / 2;
+				}
 			} else {
-				totalLinearFootage += itemLinearFootage / 2;
+				if (IsAllowedToStack(lineItems, item, truckHeightInches)) {
+					if(alreadyStacked) {
+						totalLinearFootage += itemLinearFootage / 2;
+					}
+					alreadyStacked = true;
+				} else {
+					totalLinearFootage += itemLinearFootage / 2;
+				}
 			}
 
 			firstColumn = !firstColumn;
-
 		}
 
 		// Round the result to the next whole number
@@ -55,6 +65,24 @@ public class LinearFootageOptimizer {
 	public double calculateItemLinearFootage(LineItem item) {
 		double linearFootage = (item.getLengthInches() / 12.0);
 		return linearFootage;
+	}
+	
+	public boolean IsAllowedToStack(List<LineItem> lineItems, LineItem currentItem, int truckHeightInches) {
+		if (currentItem.isStackable()) {
+			int currentIndex = lineItems.indexOf(currentItem);
+			if (currentIndex > 0) {
+				// Ensure we are not at the beginning of the list
+				LineItem itemBehind = lineItems.get(currentIndex - 1);
+
+				if (itemBehind.isStackable() && itemBehind.getWidthInches() == currentItem.getWidthInches()
+						&& (currentItem.getHeightInches() + itemBehind.getWidthInches()) < truckHeightInches) {
+					return true;
+				}
+			} else {
+				return false;
+			}
+		}
+		return false;
 	}
 
 }
